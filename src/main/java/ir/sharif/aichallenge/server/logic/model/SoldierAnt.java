af6abb792fc2d4122941baa03c8f79a01d576f87
Alireza Ziaee <alrz1999@gmail.com>
2021-02-08 19:21:53 +0330

    models almost completed.

--- src/main/java/ir/sharif/aichallenge/server/logic/model/SoldierAnt.java
+++
@@ -1,9 +0,0 @@
-package ir.sharif.aichallenge.server.logic.model;
-
-import ir.sharif.aichallenge.server.logic.config.ConstConfigs;
-
-public class SoldierAnt extends Ant {
-    public SoldierAnt() {
-        health = ConstConfigs.SOLDIER_ANT_INITIAL_HEALTH;
-    }
-}
--- src/main/java/ir/sharif/aichallenge/server/logic/model/WorkerAnt.java
+++
@@ -1,9 +0,0 @@
-package ir.sharif.aichallenge.server.logic.model;
-
-import ir.sharif.aichallenge.server.logic.config.ConstConfigs;
-
-public class WorkerAnt extends Ant {
-    public WorkerAnt() {
-        health = ConstConfigs.WORKER_ANT_INITIAL_HEALTH;
-    }
-}
