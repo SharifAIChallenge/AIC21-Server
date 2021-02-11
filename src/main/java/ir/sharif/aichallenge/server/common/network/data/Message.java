d768c1a7e4d9aa1ec154b2128f36c878a1d47d9c
m.dehghankar <mohsendehghankar@gmail.com>
2021-02-11 16:32:12 +0330

    refactor messgeTypes

--- src/main/java/ir/sharif/aichallenge/server/common/network/data/Message.java
+++ src/main/java/ir/sharif/aichallenge/server/common/network/data/Message.java
@@ -10,22 +10,18 @@ import lombok.Getter;
 public class Message {
 
     private final String type;
-    // null for server messages
-    private final String token;
     // payload = info
     private final JsonObject payload;
 
     public Message(String type, JsonObject payload, String token) {
         this.type = type;
         this.payload = payload;
-        this.token = token;
     }
 
     // for server messages
     public Message(String type, JsonObject payload) {
         this.type = type;
         this.payload = payload;
-        this.token = null;
     }
 
     public String getType() {
